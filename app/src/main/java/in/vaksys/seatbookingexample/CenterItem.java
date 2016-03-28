4fd5205e8f4f7b4a023923d094171495108a6d8b
Harsh Dalwadi <dalwadi2@gmail.com>
2016-03-28 17:59:58 +0530

    Initial commit

---
+++ app/src/main/java/in/vaksys/seatbookingexample/CenterItem.java
@@ -0,0 +1,15 @@
+package in.vaksys.seatbookingexample;
+
+public class CenterItem extends AbstractItem {
+
+    public CenterItem(String label) {
+        super(label);
+    }
+
+
+    @Override
+    public int getType() {
+        return TYPE_CENTER;
+    }
+
+}
---
+++ app/src/main/java/in/vaksys/seatbookingexample/EdgeItem.java
@@ -0,0 +1,16 @@
+package in.vaksys.seatbookingexample;
+
+public class EdgeItem extends AbstractItem {
+
+    public EdgeItem(String label) {
+        super(label);
+    }
+
+
+
+    @Override
+    public int getType() {
+        return TYPE_EDGE;
+    }
+
+}
